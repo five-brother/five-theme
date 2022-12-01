04daac1c294008c91c810d355f20165e741500da
cheneab <274092999@qq.com>
2022-12-01 16:27:57 +0800

    first commit

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<section class="error-404 not-found  p-5 m-5">
+			<header class="page-header text-center p-5 m-5">
+				<h1 class="page-title p-5 m-5">404 - 页面未找到</h1>
+			</header><!-- .page-header -->
+
+		</section><!-- .error-404 -->
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
